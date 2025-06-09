5f4656eb3779edfc9198754bce4cbcfb11a64692
Laurynas <klumbyslaurynas@gmail.com>
2025-06-09 21:01:16 +0300

    allTask

--- taskTracker/index.php
+++ taskTracker/index.php
@@ -1,27 +1,56 @@
 <?php
 
-
-// if ($argc < 2) {
-//     echo 'Nieko neivesta';
-// }
-
-function cli($cli)
+function cli($argc)
 {
-    if ($cli[0]) {
+    if ($argc < 2) {
         echo "Naudojimas:\n";
         echo "php task.php add \"užduoties tekstas\"\n";
         echo "php task.php list\n";
         echo "php task.php done [numeris]\n";
     }
 };
-$cli = $argv;
-cli($cli);
+cli($argc);
 
 
-// function add()
-// {
-//     $tasks = ['id' => 1, 'task' => 'sukurti web', 'status' => 'in-progress'];
-//     file_put_contents(__DIR__ . '/data/tasks.json', json_encode($tasks, JSON_PRETTY_PRINT));
-// }
-// add();
-// sutvarkyti kad patikrintu ar failas jau sukurtas ar dar ne
+function add($argv)
+{
+    $file = __DIR__ . '/data/tasks.json';
+
+    if (!file_exists($file)) {
+        file_put_contents(__DIR__ . '/data/tasks.json', json_encode([], JSON_PRETTY_PRINT));
+    }
+
+    $tasks = json_decode(file_get_contents(__DIR__ . '/data/tasks.json'), true);
+
+    $taskId = 1;
+    foreach ($tasks as $task) {
+        if ($task['id'] >= $taskId) {
+            $taskId++;
+        }
+    }
+    $tasks[] = [
+        'id' => $taskId,
+        'task' => $argv[1],
+        'status' => 'in-progress'
+    ];
+    file_put_contents(__DIR__ . '/data/tasks.json', json_encode($tasks, JSON_PRETTY_PRINT));
+    // print_r($tasks);
+}
+// add($argv);
+
+function allTasks()
+{
+    $tasks = json_decode(file_get_contents(__DIR__ . '/data/tasks.json'), true);
+
+    foreach ($tasks as $task) {
+        echo 'ID: ' . $task['id'] . ' TASK: ' . $task['task'] .  ' STATUS: ' . $task['status'] . "\n";
+    }
+}
+// allTasks();
+
+
+function oneTask($argv){
+    $tasks = json_decode(file_get_contents(__DIR__ . '/data/tasks.json'), true);
+
+}
+oneTask($argv);
