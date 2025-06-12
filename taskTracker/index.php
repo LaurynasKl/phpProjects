67a32ff61345b3c08ac52dbc69b37e9f5ca50e17
Laurynas <klumbyslaurynas@gmail.com>
2025-06-12 20:25:19 +0300

    bug fix

--- taskTracker/index.php
+++ taskTracker/index.php
@@ -11,32 +11,92 @@ function cli($argc)
         echo "php task.php done [numeris]\n";
     }
 };
-// if ($argc < 2) {
-//     cli($argc);
-// } else {
-//     switch ($argv[1]) {
-//         case 'add':
-//             add($argv);
-//             break;
-//         case 'update':
-//             update($argv);
-//             break;
 
-//         case 'list':
-//             listTasks($argv);
-//             break;
+if ($argc < 2) {
+    cli($argc);
+} else {
+    switch ($argv[1]) {
+        case 'add':
+            add($argv);
+            break;
 
-//         case 'oneTask':
-//             oneTask($argv);
-//             break;
+        case 'update':
+            if ($argc >= 4) {
+                update($argv);
+                break;
+            } else {
+                echo 'Something wrong';
+                break;
+            }
 
-//         case 'delete':
-//             delete($argv);
-//             break;
+        case 'list':
+            if ($argv[2] === 'done') {
+                taskStatus($argv);
+                break;
+            }
+            if ($argv[2] === 'todo') {
+                taskStatus($argv);
+                break;
+            }
+            if ($argv[2] === 'in-progress') {
+                taskStatus($argv);
+                break;
+            }
+            if ($argc === 2 && $argc < 3) {
+                listTasks($argv);
+                break;
+            } else {
+                echo 'Something wrong';
+                break;
+            }
 
-//     }
-// }
+        case 'oneTask':
+            if ($argc >= 3) {
+                oneTask($argv);
+                break;
+            } else {
+                echo 'Klaida';
+                break;
+            }
 
+        case 'delete':
+            if ($argc >= 3) {
+                delete($argv);
+                break;
+            } else {
+                echo 'blogai';
+                break;
+            }
+    }
+}
+
+function add($argv)
+{
+    $file = __DIR__ . '/data/tasks.json';
+
+    if (!file_exists($file)) {
+        file_put_contents(__DIR__ . '/data/tasks.json', json_encode([], JSON_PRETTY_PRINT));
+    }
+
+    $tasks = json_decode(file_get_contents(__DIR__ . '/data/tasks.json'), true);
+
+    $newId = 0;
+    foreach ($tasks as $task) {
+        if ($task['id'] > $newId) {
+            $newId = $task['id'];
+        }
+    }
+    $taskId = $newId + 1;
+
+    $tasks[] = [
+        'id' => $taskId,
+        'task' => implode(" ", array_splice($argv, 2)),
+        'status' => 'todo'
+    ];
+
+    file_put_contents(__DIR__ . '/data/tasks.json', json_encode($tasks, JSON_PRETTY_PRINT));
+    echo "Task added successfully (id: $taskId)";
+}
 
 function update($argv)
 {
@@ -44,57 +104,22 @@ function update($argv)
 
     foreach ($tasks as $key => $task) {
         if ($argv[1] === 'update' && $task['id'] == $argv[2]) {
-            $tasks[$key]['task'] = $argv[3];
-            echo $task['task'];
-            // echo "Task updates successfully";
+            $tasks[$key]['task'] = implode(" ", array_splice($argv, 3));
+            echo "Task updates successfully";
         }
     }
     file_put_contents(__DIR__ . '/data/tasks.json', json_encode($tasks, JSON_PRETTY_PRINT));
 }
 
-
-function add($argv)
-{
-    if ($argv[1] === 'add') {
-        $file = __DIR__ . '/data/tasks.json';
-
-        if (!file_exists($file)) {
-            file_put_contents(__DIR__ . '/data/tasks.json', json_encode([], JSON_PRETTY_PRINT));
-        }
-
-        $tasks = json_decode(file_get_contents(__DIR__ . '/data/tasks.json'), true);
-
-        $newId = 0;
-        foreach ($tasks as $task) {
-            if ($task['id'] > $newId) {
-                $newId = $task['id'];
-            }
-        }
-        $taskId = $newId + 1;
-
-        $tasks[] = [
-            'id' => $taskId,
-            'task' => $argv[2],
-            'status' => 'in-progress'
-        ];
-
-        file_put_contents(__DIR__ . '/data/tasks.json', json_encode($tasks, JSON_PRETTY_PRINT));
-        echo "Task added successfully (id: $taskId)";
-    }
-}
-
 function listTasks($argv)
 {
-    if ($argv[1] === 'list') {
-        $tasks = json_decode(file_get_contents(__DIR__ . '/data/tasks.json'), true);
+    $tasks = json_decode(file_get_contents(__DIR__ . '/data/tasks.json'), true);
 
-        foreach ($tasks as $task) {
-            echo 'ID: ' . $task['id'] . ' TASK: ' . $task['task'] .  ' STATUS: ' . $task['status'] . "\n";
-        }
+    foreach ($tasks as $task) {
+        echo 'ID: ' . $task['id'] . ' TASK: ' . $task['task'] .  ' STATUS: ' . $task['status'] . "\n";
     }
 }
 
-// patvarkyti kad jeigu nepasirenki argumento ismestu pranesima kad nepasirinktas id
 function oneTask($argv)
 {
     $tasks = json_decode(file_get_contents(__DIR__ . '/data/tasks.json'), true);
@@ -110,21 +135,33 @@ function oneTask($argv)
         exit();
     }
 }
-// oneTask($argv);
 
-// patvarkyti kad jeigu nepasirenki argumento ismestu pranesima kad nepasirinktas id
 function delete($argv)
 {
-    if ($argv[1] === 'delete') {
-        $tasks = json_decode(file_get_contents(__DIR__ . '/data/tasks.json'), true);
-        foreach ($tasks as $key => $task) {
-            if ($task['id'] === (int)$argv[2]) {
-                unset($tasks[$key]);
-            }
+    $tasks = json_decode(file_get_contents(__DIR__ . '/data/tasks.json'), true);
+    $taskDeleted = false;
+    foreach ($tasks as $key => $task) {
+        if ($argv[1] === 'delete' && $task['id'] === (int)$argv[2]) {
+            unset($tasks[$key]);
+            $taskDeleted = true;
         }
-        $tasks = array_values($tasks);
-        file_put_contents(__DIR__ . '/data/tasks.json', json_encode($tasks, JSON_PRETTY_PRINT));
+    }
+    if (!$taskDeleted) {
+        echo 'Klaida, tokios uzduoties nera';
+        exit();
+    }
+    $tasks = array_values($tasks);
+    file_put_contents(__DIR__ . '/data/tasks.json', json_encode($tasks, JSON_PRETTY_PRINT));
+}
+
+function taskStatus($argv)
+{
+
+    $tasks = json_decode(file_get_contents(__DIR__ . '/data/tasks.json'), true);
+
+    foreach ($tasks as $key => $value) {
+        if ($value['status'] === $argv[2]) {
+            echo 'ID: ' . $value['id'] . ' TASK: ' . $value['task'] .  ' STATUS: ' . $value['status'] . "\n";
+        }
     }
 }
-// delete($argv);
-
